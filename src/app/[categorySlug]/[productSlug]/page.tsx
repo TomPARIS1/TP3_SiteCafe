908718162f457bc41a2151592880b7a4b53e17e5
Tom <98466837+TomPARIS1@users.noreply.github.com>
2023-10-25 17:26:25 +0200

    Question 3.4

--- src/app/[categorySlug]/[productSlug]/page.tsx
+++ src/app/[categorySlug]/[productSlug]/page.tsx
@@ -9,35 +9,52 @@ import {
   SectionContainer,
 } from "tp-kit/components";
 import { NextPageProps } from "../../../types";
-import { PRODUCTS_CATEGORY_DATA } from "tp-kit/data";
 import { Metadata } from "next";
 import {
   ProductAttribute,
   ProductAttributesTable,
 } from "../../../components/product-attributes-table";
-import { AddToCartButton } from "../../../components/add-to-cart-button";
-const product = {
-  ...PRODUCTS_CATEGORY_DATA[0].products[0],
-  category: {
-    ...PRODUCTS_CATEGORY_DATA[0],
-    products: PRODUCTS_CATEGORY_DATA[0].products.slice(1),
-  },
-};
+import prisma from "../../../utils/prisma";
+import { notFound } from "next/navigation";
+
 
 type Props = {
   categorySlug: string;
   productSlug: string;
 };
 
+async function getProduct(slug: string) {
+  const product = await prisma.product.findUnique({
+    include: {
+      category: {
+        include: {
+          products: {
+            where: {
+              slug: {not: slug}
+            }
+          }
+        }
+      }
+    },
+    where: {
+      slug: slug
+    }
+  })
+  return product;
+}
+
 export async function generateMetadata({
-  params,
-  searchParams,
-}: NextPageProps<Props>): Promise<Metadata> {
+                                         params,
+                                         searchParams,
+                                       }: NextPageProps<Props>): Promise<Metadata> {
+  const product = await getProduct(params.productSlug)
+
+  if (!product) return {}
   return {
     title: product.name,
     description:
-      product.desc ??
-      `Succombez pour notre ${product.name} et commandez-le sur notre site !`,
+        product.desc ??
+        `Succombez pour notre ${product.name} et commandez-le sur notre site !`,
   };
 }
 
@@ -50,85 +67,88 @@ const productAttributes: ProductAttribute[] = [
 ];
 
 export default async function ProductPage({ params }: NextPageProps<Props>) {
+  const product = await getProduct(params.productSlug)
+
+  if (!product) notFound()
   return (
-    <SectionContainer wrapperClassName="max-w-5xl">
-      <BreadCrumbs
-        className="my-8"
-        items={[
-          {
-            label: "Accueil",
-            url: "/",
-          },
-          {
-            label: product.category.name,
-            url: `/${product.category.slug}`,
-          },
-          {
-            label: product.name,
-            url: `/${product.path}`,
-          },
-        ]}
-      />
+      <SectionContainer wrapperClassName="max-w-5xl">
+        <BreadCrumbs
+            className="my-8"
+            items={[
+              {
+                label: "Accueil",
+                url: "/",
+              },
+              {
+                label: product.category.name,
+                url: `/${product.category.slug}`,
+              },
+              {
+                label: product.name,
+                url: `/${product.path}`,
+              },
+            ]}
+        />
 
-      {/* Produit */}
-      <section className="flex flex-col md:flex-row justify-center gap-8">
-        {/* Product Image */}
-        <div className="relative">
-          <ProductImage
-            {...product}
-            priority
-            className="rounded-lg sticky top-12 object-cover sm:aspect-video md:aspect-auto w-full md:w-[300px]"
-          />
-        </div>
+        {/* Produit */}
+        <section className="flex flex-col md:flex-row justify-center gap-8">
+          {/* Product Image */}
+          <div className="relative">
+            <ProductImage
+                {...product}
+                priority
+                className="rounded-lg sticky top-12 object-cover sm:aspect-video md:aspect-auto w-full md:w-[300px]"
+            />
+          </div>
 
-        {/* Product body */}
-        <div className="flex-1">
-          <div className="prose prose-lg">
-            {/* Product Name */}
-            <h1>{product.name}</h1>
+          {/* Product body */}
+          <div className="flex-1">
+            <div className="prose prose-lg">
+              {/* Product Name */}
+              <h1>{product.name}</h1>
 
-            {/* Product Rating */}
-            <ProductRating value={4} size={18} />
+              {/* Product Rating */}
+              <ProductRating value={4} size={18} />
 
-            {/* Desc */}
-            <p>{product.desc}</p>
+              {/* Desc */}
+              <p>{product.desc}</p>
 
-            {/* Prix et ajout au panier */}
-            <div className="flex justify-between items-center gap-8">
-              <p className="!my-0 text-xl">
-                <FormattedPrice price={product.price} />
-              </p>
-              <AddToCartButton variant={"primary"} product={product} fullWidth={false} />
+              {/* Prix et ajout au panier */}
+              <div className="flex justify-between items-center gap-8">
+                <p className="!my-0 text-xl">
+                  <FormattedPrice price={product.price} />
+                </p>
+                <Button variant={"primary"}>Ajouter au panier</Button>
+              </div>
             </div>
+
+            {/* Products attribute */}
+            <ProductAttributesTable className="mt-6" data={productAttributes} />
           </div>
+        </section>
 
-          {/* Products attribute */}
-          <ProductAttributesTable className="mt-6" data={productAttributes} />
-        </div>
-      </section>
+        {/* Related products */}
+        <section>
+          <div className="mt-24">
+            <div className="prose prose-lg mb-8">
+              <h2>Vous aimerez aussi</h2>
+            </div>
 
-      {/* Related products */}
-      <section>
-        <div className="mt-24">
-          <div className="prose prose-lg mb-8">
-            <h2>Vous aimerez aussi</h2>
+            <ProductGridLayout products={product.category.products}>
+              {(product) => (
+                  <ProductCardLayout
+                      product={product}
+                      button={
+                        <Button variant="ghost" className="flex-1 !py-4">
+                          Ajouter au panier
+                        </Button>
+                      }
+                  />
+              )}
+            </ProductGridLayout>
           </div>
-
-          <ProductGridLayout products={product.category.products}>
-            {(product) => (
-              <ProductCardLayout
-                product={product}
-                button={
-                  <Button variant="ghost" className="flex-1 !py-4">
-                    Ajouter au panier
-                  </Button>
-                }
-              />
-            )}
-          </ProductGridLayout>
-        </div>
-      </section>
-      {/* /Related products */}
-    </SectionContainer>
+        </section>
+        {/* /Related products */}
+      </SectionContainer>
   );
 }
