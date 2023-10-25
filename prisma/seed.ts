7f099047e775bceb84a2b694dae2f66cbd794540
Tom <98466837+TomPARIS1@users.noreply.github.com>
2023-10-25 15:24:09 +0200

    Question 2.3

---
+++ prisma/seed.ts
@@ -0,0 +1,43 @@
+import { PRODUCTS_CATEGORY_DATA } from "tp-kit/data";
+
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
+async function main() {
+    await prisma.productCategory.deleteMany({})
+    await prisma.product.deleteMany({})
+
+    PRODUCTS_CATEGORY_DATA.map(async (productCateg) => {
+        const categ = await prisma.productCategory.create({
+            data: {
+                id: productCateg.id,
+                slug: productCateg.slug,
+                name: productCateg.name
+            }
+        })
+
+
+        productCateg.products.map(async (product) => {
+            const productP = await prisma.product.create({
+                data: {
+                    id: product.id,
+                    slug: product.slug,
+                    path: product.path,
+                    name: product.name,
+                    desc: product.desc,
+                    img: product.path,
+                    price: product.price,
+                    categoryId: productCateg.id,
+                }
+            })
+        })
+    })
+}
+main()
+    .then(async () => {
+        await prisma.$disconnect()
+    })
+    .catch(async (e) => {
+        console.error(e)
+        await prisma.$disconnect()
+        process.exit(1)
+    })
