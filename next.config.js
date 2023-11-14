489f1e5cb15ae6749dfd15380c4b895e56b9c518
Tom <98466837+TomPARIS1@users.noreply.github.com>
2023-11-14 17:03:33 +0100

    Question 4.2

--- next.config.js
+++ next.config.js
@@ -1,13 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'www.starbucks.fr',
-      },
-    ],
-  }
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'www.starbucks.fr',
+            },
+        ],
+        experimental: {
+            serverActions: true,
+        },
+    }
 }
 
 module.exports = nextConfig
